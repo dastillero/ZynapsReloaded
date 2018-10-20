0ef8fa2ae46e81fda16114a65031f9d6c13047c0
dastillero <daniel.astillero@gmail.com>
2018-10-20 15:28:40 +0200

    Upgrade to Unreal Engine 4.20
    Upgrade to Unreal Engine 4.20

--- Source/ZynapsReloaded.Target.cs
+++ Source/ZynapsReloaded.Target.cs
@@ -5,21 +5,9 @@ using System.Collections.Generic;
 
 public class ZynapsReloadedTarget : TargetRules
 {
-	public ZynapsReloadedTarget(TargetInfo Target)
+	public ZynapsReloadedTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-	}
-
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "ZynapsReloaded" } );
+		ExtraModuleNames.Add("ZynapsReloaded");
 	}
 }
